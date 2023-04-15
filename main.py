e038def5e655bf9fb67250a4cf2a5b735870bb20
Assan21 <akhmetov.asan@gmail.com>
2023-04-15 16:57:38 +0600

    feat: img crop to detect in specific area

--- main.py
+++ main.py
@@ -19,6 +19,16 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1120)
 img_path = r'C:\\Users\\akhme\\Desktop\\diploma\\images'
 os.chdir(img_path)
 
+
+square_start, square_end = (440, 100), (840, 650)
+
+square_x = square_start[0]
+square_y = square_start[1]
+
+square_width = square_end[0] - square_x
+square_height = square_end[1] - square_y
+
+
 def takePhoto(path: str, img: any):
     if isUnique(path):
         cv2.imwrite(path, img)
@@ -36,10 +46,23 @@ def isUnique(img):
 
 def showEllipse(img, show=True):
     if show:
+
+        isTaken = False
+
+        if cv2.waitKey(1) & 0xFF == ord('z'):
+            randId = rd.randint(1, 1500000)
+            takePhoto('image' + str(randId) + '.png', img)
+            isTaken = True
+
+            if isTaken and os.path.exists('image' + str(randId) + '.png'): 
+                took = cv2.imread('image' + str(randId) + '.png')
+                croppedTook = took[square_y: square_y + square_height, square_x: square_x + square_width]
+                cv2.imshow("CroppedTook", croppedTook)
         ellipsedImg = cv2.ellipse(img, (640, 360), (300, 200), 90, 0, 360, (0, 255, 0), 5)
+        squareImg = cv2.rectangle(ellipsedImg, (440, 100), (840, 650), (125, 125, 125), 2)
         # green_image = cv2.cvtColor(image, cv2.C)
         withText = cv2.putText(
-            ellipsedImg, 
+            squareImg, 
             'Лицо должно быть в области эллипса', 
             (50, 680), 
             cv2.FONT_HERSHEY_COMPLEX, 0.6, 
@@ -80,15 +103,16 @@ while True:
     success, img = cap.read()
     face_blurred = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     # face_located = fc.face_locations(face_blurred, model='cnn')
-    if cv2.waitKey(1) & 0xFF == ord('o'):
-        print("clicked")
-        if not isOnePerson(face_blurred) :
-            print("There has to be only one face")
+    # if cv2.waitKey(1) & 0xFF == ord('o'):
+    #     print("clicked")
+    #     if not isOnePerson(face_blurred) :
+    #         print("There has to be only one face")
         # else:
         #     showEllipse(img)
     # dfs = DeepFace.find(img_path=img, db_path="C:/Users/akhme/Desktop/diploma/images", model_name='Facenet')
     # face = DeepFace.detectFace(img)
     
+    showEllipse(img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         print("exit")
         break
@@ -98,7 +122,7 @@ while True:
         takePhoto('image' + str(rd.randint(1, 1500000)) + '.png', img)
         break
     
-    cv2.imshow("Image", img)
+    # cv2.imshow("Image", img)
 
 
 cap.release()
