04a8e3c22bb1d29e1329ee361ef999ed550ce550
Assan21 <akhmetov.asan@gmail.com>
2023-04-24 00:44:50 +0600

    feat: real time face detection

---
+++ face-rec.py
@@ -0,0 +1,190 @@
+import cv2
+import face_recognition
+from deepface import DeepFace
+import os
+from mtcnn import MTCNN
+import random as rd
+import time
+
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2080)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 2180)
+
+
+img_path = r"C:\\Users\\akhme\\Desktop\\diploma"
+os.chdir(img_path)
+
+
+square_start, square_end = (440, 100), (840, 650)
+
+square_x = square_start[0]
+square_y = square_start[1]
+
+square_width = square_end[0] - square_x
+square_height = square_end[1] - square_y
+
+
+def takePhoto(path: str, img: any):
+    if isUnique(path):
+        cv2.imwrite(path, img)
+        # cv2.destroyAllWindows()
+
+
+def deletePhoto(path):
+    os.remove(img_path + "\\" + path)
+
+
+def isUnique(img):
+    unique = True
+    for file in os.listdir(img_path):
+        if img == file:
+            unique = False
+    return unique
+
+
+def showEllipse(img, locations, show=True):
+    count = 0
+    if len(locations) == 1:
+        (top, right, bottom, left) = locations[0]
+
+        print("Face")
+        croppedTook = img[
+            square_y : square_y + square_height,
+            square_x : square_x + square_width,
+        ]
+        cv2.imshow("CroppedTook", croppedTook)
+
+        isTaken = False
+        isFound = None
+        try:
+            randId = rd.randint(1, 1500000)
+            if count == 0:
+                takePhoto("deleted/image" + str(randId) + ".png", croppedTook)
+                count += 1
+                isTaken = True
+                time.sleep(3)
+            print(count, os.path.exists("deleted/image" + str(randId) + ".png"), randId)
+            if count == 1 and os.path.exists("deleted/image" + str(randId) + ".png"):
+                took = cv2.imread("deleted/image" + str(randId) + ".png")
+                dfs = DeepFace.extract_faces(croppedTook)
+                width, height = (
+                    dfs[0]["facial_area"]["w"],
+                    dfs[0]["facial_area"]["h"],
+                )
+                size = width * height
+                distance = 100 / (size**0.5)
+                if int(distance * 100) > 30:
+                    print("far")
+                else:
+                    print("suits")
+                    isFound = True
+                print(size, distance)
+                # cv2.destroyAllWindows()
+        except:
+            print("ustudy")
+            isFound = False
+        ellipsedImg = cv2.ellipse(
+            img, (640, 360), (300, 200), 90, 0, 360, (0, 255, 0), 5
+        )
+        squareImg = cv2.rectangle(
+            ellipsedImg, (440, 100), (840, 650), (125, 125, 125), 2
+        )
+        # green_image = cv2.cvtColor(image, cv2.C)
+        font = cv2.FONT_HERSHEY_COMPLEX
+        warningText = "Лицо должно быть в области эллипса"
+        faceErrorText = "Face not found"
+        faceFoundText = "Face found"
+        warningTextXY = (50, 680)
+        errorTextXY = (550, 680)
+        foundTextXY = (750, 680)
+        thickness = 1
+
+        withText = cv2.putText(
+            squareImg,
+            warningText,
+            warningTextXY,
+            font,
+            0.6,
+            (0, 255, 0),
+            thickness,
+            cv2.LINE_AA,
+        )
+        errorText = cv2.putText(
+            squareImg,
+            faceErrorText,
+            errorTextXY,
+            font,
+            0.6,
+            (0, 0, 250),
+            thickness,
+            cv2.LINE_AA,
+        )
+
+        foundText = cv2.putText(
+            squareImg,
+            faceFoundText,
+            foundTextXY,
+            font,
+            0.6,
+            (250, 0, 0),
+            thickness,
+            cv2.LINE_AA,
+        )
+    else:
+        print("Many items")
+    cv2.imshow("Video", img)
+
+    #     print(isFound)
+    #     if isFound == False:
+    #         text_size_warning, _ = cv2.getTextSize(warningText, font, 0.6, thickness)
+    #         text_size_found, _ = cv2.getTextSize(faceFoundText, font, 0.6, thickness)
+    #         # cv2.rectangle(img, (warningTextXY[0], warningTextXY[1] - text_size_warning[1]), (warningTextXY[0] + text_size_warning[0], warningTextXY[1]), (0, 0, 0), -1)
+    #         # cv2.rectangle(img, (foundTextXY[0], foundTextXY[1] - text_size_found[1]), (foundTextXY[0] + text_size_found[0], foundTextXY[1]), (0, 0, 0), -1)
+
+    #         cv2.imshow("Face Attendace1", img)
+    #     elif isFound == None:
+    #         text_size_error, _ = cv2.getTextSize(faceErrorText, font, 0.6, thickness)
+    #         text_size_found, _ = cv2.getTextSize(faceFoundText, font, 0.6, thickness)
+    #         # cv2.rectangle(img, (errorTextXY[0], errorTextXY[1] - text_size_error[1]), (errorTextXY[0] + text_size_error[0], errorTextXY[1]), (0, 0, 0), -1)
+    #         # cv2.rectangle(img, (foundTextXY[0], foundTextXY[1] - text_size_found[1]), (foundTextXY[0] + text_size_found[0], foundTextXY[1]), (0, 0, 0), -1)
+
+    #         cv2.imshow("Face Attendace2", img)
+    #     else:
+    #         text_size_error, _ = cv2.getTextSize(faceErrorText, font, 0.6, thickness)
+    #         text_size_warning, _ = cv2.getTextSize(warningText, font, 0.6, thickness)
+    #         # cv2.rectangle(img, (warningTextXY[0], warningTextXY[1] - text_size_error[1]), (warningTextXY[0] + text_size_error[0], warningTextXY[1]), (0, 0, 0), -1)
+    #         # cv2.rectangle(img, (errorTextXY[0], errorTextXY[1] - text_size_found[1]), (errorTextXY[0] + text_size_found[0], errorTextXY[1]), (0, 0, 0), -1)
+
+    #         cv2.imshow("Face Attendace3", img)
+
+
+# def isOnePerson(img: any) -> bool:
+#     detector = MTCNN()
+#     detections = detector.detect_faces(img)
+#     print("Length", len(detections))
+#     return len(detections) == 1
+
+
+while True:
+    ret, frame = cap.read()
+
+    # rgb_frame = frame[:, :, ::-1]
+
+    # face_locations = face_recognition.face_locations(frame)
+
+    # for top, right, bottom, left in face_locations:
+    #     cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+    mirrored_img = cv2.flip(frame, 1)
+
+    showEllipse(mirrored_img, [], True)
+    # cv2.imshow("Video", mirrored_img)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# Освобождение ресурсов
+cap.release()
+# cv2.destroyAllWindows()
+
+# len(face_located) == 1 and left >= square_x and left < square_end[0] and right > square_x and right <= square_end[0] and top >= square_start[1] and top < square_end[1] and bottom <= square_end[1] and bottom > square_start[1]
--- main.py
+++ main.py
@@ -4,11 +4,9 @@ import os
 from deepface import DeepFace
 import numpy as np
 import time
-# import dlib
+
 import face_recognition as fc
 from mtcnn import MTCNN
-# import cmake
-# import dlib
 
 # cv2.Vide
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -16,7 +14,7 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1080)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1120)
 
 
-img_path = r'C:\\Users\\akhme\\Desktop\\diploma\\images'
+img_path = r"C:\\Users\\akhme\\Desktop\\diploma\\images"
 os.chdir(img_path)
 
 
@@ -34,8 +32,10 @@ def takePhoto(path: str, img: any):
         cv2.imwrite(path, img)
         cv2.destroyAllWindows()
 
+
 def deletePhoto(path):
-    os.remove(img_path + '\\' + path)
+    os.remove(img_path + "\\" + path)
+
 
 def isUnique(img):
     unique = True
@@ -44,93 +44,107 @@ def isUnique(img):
             unique = False
     return unique
 
-def showEllipse(img, show=True):
-    if show:
 
+def showEllipse(img, show=True):
+    new_img = img
+    # face_located = fc.face_locations(img)
+
+    try:
         isTaken = False
         isFound = None
 
-        if cv2.waitKey(1) & 0xFF == ord('z'):
-            randId = rd.randint(1, 1500000)
-            takePhoto('image' + str(randId) + '.png', img)
-            isTaken = True
+        # if cv2.waitKey(1) & 0xFF == ord("z"):
+        # randId = rd.randint(1, 1500000)
+        # takePhoto("image" + str(randId) + ".png", img)
+        # isTaken = True
 
-            if isTaken and os.path.exists('image' + str(randId) + '.png'): 
-                took = cv2.imread('image' + str(randId) + '.png')
-                croppedTook = took[square_y: square_y + square_height, square_x: square_x + square_width]
-                cv2.imshow("CroppedTook", croppedTook)
+        # if isTaken and os.path.exists("image" + str(randId) + ".png"):
+        # took = cv2.imread("image" + str(randId) + ".png")
+        # croppedTook = took[
+        #     square_y : square_y + square_height,
+        #     square_x : square_x + square_width,
+        # ]
+        # cv2.imshow("CroppedTook", croppedTook)
 
-                try:
-                    dfs = DeepFace.extract_faces(croppedTook)
-                    isFound = True
-                    width, height = dfs[0]['facial_area']['w'], dfs[0]['facial_area']['h'] 
-                    size = width*height
-                    distance = 100 / (size**0.5)
-                    if int(distance*100) > 30:
-                        print('far')
-                    else:
-                        print("suits")
-                    print(dfs, size, distance)
-                except:
-                    print("ustudy")
-                    isFound = False
-                    
-        ellipsedImg = cv2.ellipse(img, (640, 360), (300, 200), 90, 0, 360, (0, 255, 0), 5)
-        squareImg = cv2.rectangle(ellipsedImg, (440, 100), (840, 650), (125, 125, 125), 2)
+        try:
+            dfs = DeepFace.extract_faces(img)
+            isFound = True
+            width, height = (
+                dfs[0]["facial_area"]["w"],
+                dfs[0]["facial_area"]["h"],
+            )
+            size = width * height
+            distance = 100 / (size**0.5)
+            if int(distance * 100) > 30:
+                print("far")
+                isFound = "Closer"
+            else:
+                print("suits")
+                isFound = True
+            # print(dfs, size, distance)
+        except:
+            print("ustudy")
+            isFound = False
+
+        ellipsedImg = cv2.ellipse(
+            img, (640, 360), (300, 200), 90, 0, 360, (0, 255, 0), 5
+        )
+        squareImg = cv2.rectangle(
+            ellipsedImg, (440, 100), (840, 650), (125, 125, 125), 2
+        )
         # green_image = cv2.cvtColor(image, cv2.C)
-        withText = cv2.putText(
-            squareImg, 
-            'Лицо должно быть в области эллипса', 
-            (50, 680), 
-            cv2.FONT_HERSHEY_COMPLEX, 0.6, 
-            (0, 255, 0), 1, 
-            cv2.LINE_AA
-        )
-        errorText = cv2.putText(
-            squareImg, 
-            'Face not found', 
-            (50, 680), 
-            cv2.FONT_HERSHEY_COMPLEX, 0.6, 
-            (0, 0, 250), 1, 
-            cv2.LINE_AA
-        )
+        font = cv2.FONT_HERSHEY_COMPLEX
+        warningText = "Лицо должно быть в области эллипса"
+        faceErrorText = "Лицо не найдено"
+        faceFoundText = "Лицо найдено"
+        closerFaceText = "Встаньте ближе"
+        warningTextXY = (50, 680)
+        errorTextXY = (550, 680)
+        foundTextXY = (750, 680)
+        thickness = 1
 
-        foundText = cv2.putText(
-            squareImg, 
-            'Face found', 
-            (50, 680), 
-            cv2.FONT_HERSHEY_COMPLEX, 0.6, 
-            (0, 0, 250), 1, 
-            cv2.LINE_AA
-        )
-        if isFound == False:
-            cv2.imshow("Face Attendace", errorText)
-        elif isFound == None:
-            cv2.imshow("Face Attendace", withText)
+        print(isFound)
+        if isFound == None or isFound == False:
+            cv2.putText(
+                squareImg,
+                warningText,
+                warningTextXY,
+                font,
+                0.6,
+                (0, 0, 0),
+                thickness,
+                cv2.LINE_AA,
+            )
+
+            # cv2.imshow("Face Attendace2", img)
+        elif isFound == "Closer":
+            cv2.putText(
+                squareImg,
+                closerFaceText,
+                warningTextXY,
+                font,
+                0.6,
+                (250, 0, 0),
+                thickness,
+                cv2.LINE_AA,
+            )
+            # cv2.imshow("Face Attendace Closer", img)
         else:
-            cv2.imshow("Face Attendace", withText)
-    else:
-        if cv2.waitKey(1) & 0xFF == ord('p'):
-            img1 = DeepFace.extract_faces(img)
-            x = img1[0]['facial_area']['x']
-            w = img1[0]['facial_area']['w']
-            y = img1[0]['facial_area']['y']
-            h = img1[0]['facial_area']['h']
-            normalized = img1[0]['face']
-            randId = rd.randint(1, 1500000)
-            takePhoto('image' + str(randId) + '.png', img)
-            print(os.path.exists('image' + str(randId) + '.png'))
-            time.sleep(3)
-            takenPhoto = cv2.imread('image' + str(randId) + '.png')
-            # takenPhoto = cv2.imread('')
-            # deletePhoto('image' + str(randId) + '.png')
-            # res = DeepFace.find()
-            cropped = takenPhoto[x: x + w][y: y + h]
-            dfs = DeepFace.find(cropped, db_path='C:/Users/akhme/Desktop/diploma/images')
-            print(cropped, dfs)
-            cv2.destroyAllWindows()
+            cv2.putText(
+                squareImg,
+                faceFoundText,
+                warningTextXY,
+                font,
+                0.6,
+                (0, 250, 0),
+                thickness,
+                cv2.LINE_AA,
+            )
+            # cv2.imshow("Face Attendace3", img)
+        cv2.imshow("Dzhigi", img)
+    except:
+        print("many faces")
 
-        cv2.imshow("Face Attendace", img)
 
 def isOnePerson(img: any) -> bool:
     detector = MTCNN()
@@ -138,31 +152,34 @@ def isOnePerson(img: any) -> bool:
     print("Length", len(detections))
     return len(detections) == 1
 
+
 while True:
     success, img = cap.read()
-    face_blurred = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     mirrored_img = cv2.flip(img, 1)
-    # face_located = fc.face_locations(face_blurred, model='cnn')
     # if cv2.waitKey(1) & 0xFF == ord('o'):
     #     print("clicked")
     #     if not isOnePerson(face_blurred) :
     #         print("There has to be only one face")
-        # else:
-        #     showEllipse(img)
+    # else:
+    #     showEllipse(img)
     # dfs = DeepFace.find(img_path=img, db_path="C:/Users/akhme/Desktop/diploma/images", model_name='Facenet')
-    # face = DeepFace.detectFace(img)
-    
-    showEllipse(mirrored_img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    edited_img = mirrored_img
+    try:
+        showEllipse(mirrored_img)
+    except:
+        print("not found")
+
+    # cv2.imshow("Image", mirrored_img)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         print("exit")
         break
 
-    if cv2.waitKey(1) & 0xFF == ord('t'):
+    if cv2.waitKey(1) & 0xFF == ord("t"):
         print("taken")
-        takePhoto('image' + str(rd.randint(1, 1500000)) + '.png', img)
+        takePhoto("image" + str(rd.randint(1, 1500000)) + ".png", img)
         break
-    
-    # cv2.imshow("Image", img)
+
+    # cv2.imshow("Image", mirrored_img)
 
 
 cap.release()
